11536de795c0ebd50f2a9ce08e18aafa876b262f
Joel Earps <joel.earps@zenobe.com>
2023-05-02 13:10:56 +0100

    feat: Hello World Realistic

---
+++ HelloWorld/lib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lib.h"
+
+void print_hello_world()
+{
+    std::cout << "Hello World\n";
+}
--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "lib.h"
 
 int main()
 {
-    std::cout << "Hello World\n";
+    print_hello_world();
 
     return 0;
 }
