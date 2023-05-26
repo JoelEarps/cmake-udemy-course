20ae8b7e2b2e1823280bc65666ef6b0d156049ef
Joel Earps <joel.earps@zenobe.com>
2023-05-26 15:15:06 +0100

    feat: Configured files

--- HelloWorld/src/lib.cpp
+++ HelloWorld/src/lib.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "lib.h"
+#include "config.hpp"
 
 void print_hello_world()
 {
     std::cout << "Hello Tanvir\n";
+    std::cout << "Project Name: " << project_name << std::endl;
+    std::cout << "Project Version: " << project_version << std::endl;
 }
