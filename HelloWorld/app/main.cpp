df274fe6a7bb9dea57f6e1406cadbdcfbc482196
Joel Earps <joel.earps@zenobe.com>
2023-09-15 17:19:10 +0100

    feat: FetchContent

--- HelloWorld/app/main.cpp
+++ HelloWorld/app/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "lib.h"
-#include <nlohmann/json.hpp>
 
 int main()
 {
     print_hello_world();
 
-    std::cout << "JSON Lib Version: " << NLOHMANN_JSON_VERSION_MAJOR << NLOHMANN_JSON_VERSION_MINOR << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+
 
     return 0;
 }
--- HelloWorld/src/lib.cpp
+++ HelloWorld/src/lib.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "lib.h"
 #include "config.hpp"
+#include <nlohmann/json.hpp>
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+
 
 void print_hello_world()
 {
-    std::cout << "Hello Tanvir\n";
-    std::cout << "Project Name: " << project_name << std::endl;
-    std::cout << "Project Version: " << project_version << std::endl;
+    std::cout << "JSON Lib Version: " << NLOHMANN_JSON_VERSION_MAJOR << NLOHMANN_JSON_VERSION_MINOR << NLOHMANN_JSON_VERSION_PATCH << std::endl;
+    std::cout << "FMT Lib Version: " << FMT_VERSION << std::endl;
+    std::cout << "CXOPPTS Lib Version: " << CXXOPTS__VERSION_MAJOR << CXXOPTS__VERSION_MINOR << CXXOPTS__VERSION_PATCH << std::endl;
+    std::cout << "SDPLOG Lib Version: " << SPDLOG_VER_MAJOR << SPDLOG_VER_MINOR << SPDLOG_VER_PATCH << std::endl;
 }
